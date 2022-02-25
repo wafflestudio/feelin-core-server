cb79f388ff4ab2e496ac2eca3e77c954cac31fcc
Hyeonseok Jung <hsjames0248@gmail.com>
2022-02-25 23:10:46 +0900

    [Fix] Restructure project in a more Nest way

--- src/apis/melon/index.ts
+++
@@ -1,6 +0,0 @@
-import searchTrackByInfo from './searchTrackByInfo';
-import login from './login';
-import getMyRecentTracks from './getMyRecentTracks';
-import createPlaylist from './createPlaylist';
-
-export { searchTrackByInfo, login, getMyRecentTracks, createPlaylist };
