64ff6da0637abe9cbcb4a69e33cf5a4e48919d97
Hyeonseok Jung <hsjames0248@gmail.com>
2022-02-25 23:19:11 +0900

    Add prefix to API and setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import setupSwagger from './utils/setupSwagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api/v1');
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+        }),
+    );
+    setupSwagger(app);
     await app.listen(3000);
 }
 bootstrap();
---
+++ src/utils/setupSwagger.ts
@@ -0,0 +1,17 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const setupSwagger = (app: INestApplication): void => {
+    const options = new DocumentBuilder()
+        .setTitle('Streaming Service API')
+        .setDescription(
+            'API for interacting with third-party music streaming services',
+        )
+        .setVersion('0.1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-docs', app, document);
+};
+
+export default setupSwagger;
