8a6078399b1c1720b961ff2a3d3d329cfdfe610c
Hyeonseok Jung <hsjames0248@gmail.com>
2022-03-23 21:45:12 +0900

    Change function to match abstract class method

--- src/track/functions/melon/index.ts
+++ src/track/functions/melon/index.ts
@@ -1,5 +1,21 @@
-import getFirstRecentTracks from './getFirstRecentTracks';
+import { AuthData, TrackInfo } from 'src/types';
+import TrackManager from '../TrackManager';
 import scrapeMyMusicTrack from './scrapeMyMusicTrack';
 import scrapeTrack from './scrapeTrack';
+import searchTrack from './searchTrack';
 
-export default { getFirstRecentTracks, scrapeTrack, scrapeMyMusicTrack };
+class MelonTrackManager extends TrackManager {
+    async searchTrack(track: TrackInfo): Promise<TrackInfo[]> {
+        return searchTrack(track);
+    }
+
+    async getMyRecentTracks(authToken: AuthData) {}
+}
+
+// Functions specific to the melon service
+const MelonTrackUtils = {
+    scrapeMyMusicTrack,
+    scrapeTrack,
+};
+
+export { MelonTrackManager, MelonTrackUtils };
--- src/track/functions/melon/searchTrack.ts
+++ src/track/functions/melon/searchTrack.ts
@@ -3,27 +3,28 @@ import cheerio from 'cheerio';
 import { TrackInfo } from 'src/types';
 import scrapeTrack from './scrapeTrack';
 
-async function searchTrack(trackInfo: TrackInfo): Promise<TrackInfo | void> {
-    const melonURL = 'https://www.melon.com/search/song/index.htm?';
+const melonURL = 'https://www.melon.com/search/song/index.htm?';
+
+async function searchTrack(track: TrackInfo): Promise<TrackInfo[]> {
+    // Melon search API limits max 50 results at once
     const response = await axios.get(melonURL, {
         params: {
             startIndex: 1,
             pagesize: 50,
-            q: trackInfo.title,
+            q: track.title,
             sort: 'weight',
             section: 'song',
             mwkLogType: 'T',
         },
     });
     const $ = cheerio.load(response.data);
-    let trackSearchResult: TrackInfo;
+    let trackList: TrackInfo[] = [];
     $('table > tbody > tr').each((_, el) => {
-        trackSearchResult = scrapeTrack($, el);
-        if (trackInfo.isEqual(trackSearchResult)) {
-            return false;
-        }
+        const trackInfo = scrapeTrack($, el);
+        trackList.push(trackInfo);
     });
-    return trackSearchResult;
+
+    return trackList;
 }
 
 export default searchTrack;
