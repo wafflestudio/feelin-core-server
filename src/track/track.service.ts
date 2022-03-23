506fb13b18f82e096e951fbe09748d535d48af72
Hyeonseok Jung <hsjames0248@gmail.com>
2022-03-23 21:46:12 +0900

    Add track matching algorithm (v0.1.0)

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { StreamServiceEnum, TrackInfo } from 'src/types';
+import { matchTrack, trackManagers } from './functions';
+import { StreamTrack, Track } from './track.entity';
 
 @Injectable()
-export class TrackService {}
+export class TrackService {
+    async getMatchingTracks(track: Track): Promise<StreamTrack[]> {
+        const { streamTracks } = track;
+        const reference = TrackInfo.fromEntity(track);
+
+        const searchPromise: Promise<TrackInfo[]>[] = [];
+        for (const streamType of StreamServiceEnum) {
+            if (streamType === streamTracks[0].streamType) {
+                continue;
+            }
+            searchPromise.push(
+                trackManagers[streamType]?.searchTrack(reference),
+            );
+        }
+        const searchResults = await Promise.all(searchPromise);
+
+        const matchPromise: Promise<StreamTrack>[] = [];
+        for (const searchResult of searchResults) {
+            const match = matchTrack(searchResult, reference);
+            matchPromise.push(match);
+        }
+        const matches = await Promise.all(matchPromise);
+        return matches;
+    }
+}
