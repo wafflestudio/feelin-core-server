7ec9fa719d1cfb94bd98789e0fe80c8743004ff5
Hyeonseok Jung <hsjames0248@gmail.com>
2022-04-01 17:43:28 +0900

    Revert back to commonjs and use dynamic import

--- src/track/functions/matchTracks.ts
+++ src/track/functions/matchTracks.ts
@@ -1,4 +1,3 @@
-import { nGram } from 'n-gram';
 import { TrackInfo } from 'src/types';
 import { StreamTrack } from '../track.entity';
 
@@ -10,7 +9,13 @@ import { StreamTrack } from '../track.entity';
  * @returns Matching TrackInfo
  */
 
-function makeNGramSet(str: string, min: number, max: number): Set<string> {
+// TODO: Come up with a better solution rather than dynamic imports
+async function makeNGramSet(
+    str: string,
+    min: number,
+    max: number,
+): Promise<Set<string>> {
+    const { nGram } = await import('n-gram');
     let nGrams = [];
     for (let i = min; i <= max; i++) {
         nGrams = [...nGrams, ...nGram(i)(str)];
@@ -18,7 +23,7 @@ function makeNGramSet(str: string, min: number, max: number): Set<string> {
     return new Set(nGrams);
 }
 
-function intersect<T extends any>(a: Set<T>, b: Set<T>): Set<T> {
+function intersect<T>(a: Set<T>, b: Set<T>): Set<T> {
     const intersect = new Set<T>();
     for (const x of a) {
         if (b.has(x)) {
@@ -28,14 +33,14 @@ function intersect<T extends any>(a: Set<T>, b: Set<T>): Set<T> {
     return intersect;
 }
 
-function jaccardSimilarity<T extends any>(a: Set<T>, b: Set<T>): number {
+function jaccardSimilarity<T>(a: Set<T>, b: Set<T>): number {
     const intersectSize = intersect(a, b).size;
     return intersectSize / (a.size + b.size - intersectSize);
 }
 
 const MIN_NGRAM = 1;
-const MAX_NGRAM = 6;
-const THRESHOLD = 0.5;
+const MAX_NGRAM = 4;
+const THRESHOLD = 0.1;
 
 async function matchTracks(
     candidates: TrackInfo[],
@@ -55,17 +60,29 @@ async function matchTracks(
                 score: 1.0,
             };
         });
-        const titleNGrams = makeNGramSet(reference.title, MIN_NGRAM, MAX_NGRAM);
-        const albumNGrams = makeNGramSet(reference.album, MIN_NGRAM, MAX_NGRAM);
+        const titleNGrams = await makeNGramSet(
+            reference.title,
+            MIN_NGRAM,
+            MAX_NGRAM,
+        );
+        const albumNGrams = await makeNGramSet(
+            reference.album,
+            MIN_NGRAM,
+            MAX_NGRAM,
+        );
         for (const track of scores) {
             track.score *= Math.max(
                 jaccardSimilarity(
                     titleNGrams,
-                    makeNGramSet(track.trackInfo.title, MIN_NGRAM, MAX_NGRAM),
+                    await makeNGramSet(
+                        track.trackInfo.title,
+                        MIN_NGRAM,
+                        MAX_NGRAM,
+                    ),
                 ),
                 jaccardSimilarity(
                     titleNGrams,
-                    makeNGramSet(
+                    await makeNGramSet(
                         track.trackInfo.titleNoParan,
                         MIN_NGRAM,
                         MAX_NGRAM,
@@ -82,11 +99,15 @@ async function matchTracks(
             track.score *= Math.max(
                 jaccardSimilarity(
                     albumNGrams,
-                    makeNGramSet(track.trackInfo.album, MIN_NGRAM, MAX_NGRAM),
+                    await makeNGramSet(
+                        track.trackInfo.album,
+                        MIN_NGRAM,
+                        MAX_NGRAM,
+                    ),
                 ),
                 jaccardSimilarity(
                     albumNGrams,
-                    makeNGramSet(
+                    await makeNGramSet(
                         track.trackInfo.albumNoParan,
                         MIN_NGRAM,
                         MAX_NGRAM,
@@ -98,6 +119,7 @@ async function matchTracks(
         scores.sort((a, b) => b.score - a.score);
         if (scores[0].score < THRESHOLD) {
             console.log(scores[0]);
+            console.log(scores[1]?.score);
             console.log(reference);
             return null;
         }
