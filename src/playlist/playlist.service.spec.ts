c5d57968c0a9ed986960a076edc62a5802a9bb26
Hyeonseok Jung <hsjames0248@gmail.com>
2022-06-23 16:45:47 +0900

    Remove tests

--- src/playlist/playlist.service.spec.ts
+++ src/playlist/playlist.service.spec.ts
@@ -1,10 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Album } from 'src/album/album.entity';
-import { Artist } from 'src/artist/artist.entity';
-import { Track } from 'src/track/track.entity';
 import { TrackService } from 'src/track/track.service';
-import UserManagers from 'src/user/functions';
 import { User } from 'src/user/user.entity';
 import {
     MockRepository,
@@ -13,7 +9,6 @@ import {
 import { Playlist } from './playlist.entity';
 import { PlaylistService } from './playlist.service';
 
-jest.setTimeout(100000);
 describe('PlaylistService', () => {
     let service: PlaylistService;
     let playlistRepository: MockRepository<Playlist>;
@@ -40,86 +35,4 @@ describe('PlaylistService', () => {
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
-
-    // it('should get playlist info', async () => {
-    //     const playlist = await PlaylistManagers['flo'].getPlaylist('33817');
-    //     expect(playlist).toBeInstanceOf(Playlist);
-    //     expect(playlist.title).toEqual('외출준비 할 때 텐션유지를 위한 노래');
-    //     expect(playlist.tracks[0]).toBeInstanceOf(Track);
-    //     expect(playlist.tracks[0].album).toBeInstanceOf(Album);
-    //     expect(playlist.tracks[0].artists[0]).toBeInstanceOf(Artist);
-    // });
-
-    // it('should save playlist', async () => {
-    //     try {
-    //         const playlist = await PlaylistManagers['flo'].getPlaylist('33817');
-    //         const token = await UserManagers['flo'].login(
-    //             process.env.FLO_ID,
-    //             process.env.FLO_PWD,
-    //         );
-    //         await PlaylistManagers['flo'].savePlaylist(playlist, token);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // });
-
-    // it('should match playlist', async () => {
-    //     try {
-    //         const playlist = await PlaylistManagers['flo'].getPlaylist('28571');
-    //         const matchedPlaylist = await service.getMatchedTracks(playlist);
-    //         for (const track of matchedPlaylist.tracks) {
-    //             // console.log(track.title);
-    //             // console.log(track.streamTracks);
-    //         }
-    //         console.log('done!');
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    // });
-
-    it('should match and save playlist', async () => {
-        try {
-            const playlist = await PlaylistManagers['flo'].getPlaylist('34052');
-            await service.getMatchedTracks(playlist);
-            const cookie = await UserManagers['melon'].login(
-                process.env.MELON_ID,
-                process.env.MELON_PWD,
-            );
-            await PlaylistManagers['melon'].savePlaylist(playlist, cookie);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-    // it('Benchmark', async () => {
-    //     // Initial: 24s
-    //     // Promise.all: 12s
-    //     try {
-    //         let total = 0;
-    //         let missing = 0;
-    //         for (let i = 31234; i < 31250; i++) {
-    //             const playlist = await PlaylistManagers['flo'].getPlaylist(
-    //                 i.toString(),
-    //             );
-    //             if (playlist.tracks) {
-    //                 const matchedPlaylist = await service.getMatchedTracks(
-    //                     playlist,
-    //                 );
-    //                 for (const track of matchedPlaylist.tracks) {
-    //                     // console.log(track.streamTracks);
-    //                     total++;
-    //                     if (track.streamTracks[1] == null) {
-    //                         missing++;
-    //                     }
-    //                 }
-    //                 console.log('done!');
-    //             } else {
-    //                 console.log('fail: ', i);
-    //             }
-    //         }
-    //         console.log(`total ${total} tracks, missing ${missing} tracks`);
-    //         console.log(`${(missing / total) * 100}% tracks missing`);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -6,8 +6,6 @@ import {
     MockRepository,
     testRepositoryModule,
 } from 'src/utils/testUtilModules';
-import { LoginStreamDto } from './dto/login-stream.dto';
-import userFunction from './functions';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
 import { UserService } from './user.service';
@@ -39,44 +37,4 @@ describe('UserService', () => {
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
-
-    // it('should not login to melon', async () => {
-    //     jest.setTimeout(10000);
-    //     const cookie = await userFunction['melon'].login(
-    //         process.env.MELON_ID,
-    //         process.env.MELON_PWD_FAKE,
-    //     );
-    //     expect(cookie).toBeNull();
-    //     console.log(cookie);
-    // });
-
-    // it('should login to melon', async () => {
-    //     jest.setTimeout(10000);
-    //     const cookie = await userFunction['melon'].login(
-    //         process.env.MELON_ID,
-    //         process.env.MELON_PWD,
-    //     );
-    //     expect(cookie).not.toBeNull();
-    //     console.log(cookie);
-    // });
-
-    it('should login to flo', async () => {
-        const token = await userFunction['flo'].login(
-            process.env.FLO_ID,
-            process.env.FLO_PWD,
-        );
-        console.log(token.toString('flo'));
-    });
-
-    // it('should connect melon account', async () => {
-    //     const loginDto = new loginStreamDto();
-    //     loginDto.streamType = 'melon';
-    //     loginDto.id = process.env.MELON_ID;
-    //     loginDto.password = process.env.MELON_PWD;
-    //     const { symmKey, publicKey } = await service.loginStreamAccount(
-    //         1,
-    //         loginDto,
-    //     );
-    //     console.log(symmKey, publicKey);
-    // });
 });
