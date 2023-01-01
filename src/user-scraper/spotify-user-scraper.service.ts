918c836617d6e816b4a1ce8c4c923365e67e1830
jm020827 <jmsmlove02@naver.com>
2023-01-01 20:02:36 +0900

    feat : Add Spotify user scraper

--- src/user-scraper/spotify-user-scraper.service.ts
+++ src/user-scraper/spotify-user-scraper.service.ts
@@ -1,11 +1,34 @@
-import { Authdata } from '@/authdata/types.js';
+import { Authdata, SpotifyAuthdata, SpotifyAuthdataKeys } from '@/authdata/types.js';
 import { Injectable } from '@nestjs/common';
 import { UserScraper } from './user-scraper.js';
+import axios from 'axios';
 
 @Injectable()
 export class SpotifyUserScraper implements UserScraper {
-    login(id: string, password: string): Promise<Authdata> {
-        // TODO: Implement me
-        return null;
+    private readonly loginUrl = 'https://api.spotify.com/v1/login';
+
+    async login(username: string, password: string): Promise<SpotifyAuthdata | null> {
+        const res = await axios.post(
+            this.loginUrl,
+            JSON.stringify({
+                username: username,
+                password: password
+            }),
+            {
+                headers: {
+                    'Authorization' : 
+                },
+            },
+        );
+        /*if (res.data?.code !== '2000000') {
+            return null;
+        }*/
+
+        const { accessToken } = res.data?.data || {}; //is there data in data again?
+        if (accessToken === undefined) {
+            return null;
+        }
+
+        return { accessToken };
     }
 }
