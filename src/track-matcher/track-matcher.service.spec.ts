566d2db080748f1218923698df09f920170b51ee
Hyeonseok Jung <hsjames0248@gmail.com>
2023-01-07 00:37:23 +0900

    style: Run lint

--- src/track-matcher/track-matcher.service.spec.ts
+++ src/track-matcher/track-matcher.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TrackMatcherService } from './track-matcher.service';
 
 describe('TrackMatcherService', () => {
-  let service: TrackMatcherService;
+    let service: TrackMatcherService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TrackMatcherService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [TrackMatcherService],
+        }).compile();
 
-    service = module.get<TrackMatcherService>(TrackMatcherService);
-  });
+        service = module.get<TrackMatcherService>(TrackMatcherService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,22 +1,8 @@
-import { TrackSearchResultDto } from './../playlist/dto/track-search-result.dto';
 import _ from 'lodash-es';
+import { TrackSearchResultDto } from './../playlist/dto/track-search-result.dto';
 import { ITrack } from './types.js';
 
 export type ValuesToArray<T> = { [P in keyof T]: T[P][] };
-
-export function collectToObject<T>(objArray: T[]): ValuesToArray<T> {
-    return objArray.reduce((newObj: ValuesToArray<T>, obj: T): ValuesToArray<T> => {
-        (Object.keys(obj) as (keyof T)[]).forEach((key, _) => {
-            if (newObj[key] === undefined) {
-                newObj[key] = [obj[key]];
-            } else {
-                // FIXME: Not a great way to modify the object??
-                newObj[key].push(obj[key]);
-            }
-        });
-        return newObj;
-    }, {} as ValuesToArray<T>);
-}
 
 export function isExactMatch(a: ITrack, b: TrackSearchResultDto): boolean {
     return (
