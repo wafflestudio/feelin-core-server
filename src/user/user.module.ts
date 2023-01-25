0116c9a9e4b42736d731da291a56122150e0aba3
Hyeonseok Jung <hsjames0248@gmail.com>
2023-01-26 00:15:14 +0900

    refactor: Remove unused dependency

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,7 +4,6 @@ import { PrismaService } from '@/prisma.service.js';
 import { UserScraperModule } from '@/user-scraper/user-scraper.module.js';
 import { CipherUtilService } from '@/utils/cipher-util/cipher-util.service.js';
 import { Module, forwardRef } from '@nestjs/common';
-import { VendorAccountRepository } from '../vendor-account/vendor-account.repository.js';
 import { UserController } from './user.controller.js';
 import { UserRepository } from './user.repository.js';
 import { UserService } from './user.service.js';
@@ -12,7 +11,7 @@ import { UserService } from './user.service.js';
 @Module({
     imports: [forwardRef(() => PlaylistModule), forwardRef(() => AuthModule), UserScraperModule],
     controllers: [UserController],
-    providers: [UserService, CipherUtilService, PrismaService, UserRepository, VendorAccountRepository],
+    providers: [UserService, CipherUtilService, PrismaService, UserRepository],
     exports: [UserService, UserRepository],
 })
 export class UserModule {}
