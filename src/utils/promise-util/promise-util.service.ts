cf4b7ab6c6e655c09141f15dd12481e427627934
Hyeonseok Jung <hsjames0248@gmail.com>
2023-02-08 01:51:38 +0900

    fix: Fix convert playlist batch (#48)
    * fix: Fix batch promise logic
    
    * fix: Fix when there is no album cover

--- src/utils/promise-util/promise-util.service.ts
+++ src/utils/promise-util/promise-util.service.ts
@@ -1,11 +1,11 @@
 import { chunk } from 'lodash-es';
 
 export class PromiseUtil {
-    static async promiseAllBatched<T>(promiseList: Promise<T>[], batchSize: number): Promise<T[]> {
+    static async promiseAllBatched<T, U>(promiseList: T[], mappingFunc: (arg: T) => Promise<U>, batchSize: number): Promise<U[]> {
         const batchedPromiseList = chunk(promiseList, batchSize);
-        const result: T[] = [];
+        const result: U[] = [];
         for (const batch of batchedPromiseList) {
-            const batchResult = await Promise.all(batch);
+            const batchResult = await Promise.all(batch.map(mappingFunc));
             result.push(...batchResult);
         }
         return result;
